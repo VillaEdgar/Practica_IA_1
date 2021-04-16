3001614445bf5eeaae06774ea05e2a1bc756dbb9
Daniel Murguia Jimenez <danmur@pop-os.localdomain>
2021-04-16 04:24:19 -0500

    generacion de matrices mejorada

--- dibujo_con_comas.py
+++ dibujo_con_comas.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 BLACK = (0, 0, 0)
 
 blueP = (20, 34, 238)
@@ -13,8 +12,6 @@ purpleP = (102,0,102)
 whiteP = (255,255,255)
 brownP = (102,51,0)
 
-colAl = (random.randrange(1, 255), random.randrange(1, 255), random.randrange(1, 255))
-
 sizeSquare = 40
 sizeCuadricula = 10
 count=0
@@ -26,55 +23,56 @@ py = 1
 x = 0
 y = 0
 pygame.init()
-size = (sizeSquare*sizeCuadricula, sizeSquare*sizeCuadricula)
-screen = pygame.display.set_mode(size)
+tamaño_ventana = (sizeSquare*sizeCuadricula, sizeSquare*sizeCuadricula)
+ventana = pygame.display.set_mode(tamaño_ventana)
 pygame.display.set_caption("Grid on PYGAME")
 clock = pygame.time.Clock()
 gameOver = False
+
 
 while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
-    fichero=open('mapa2.txt','r')
-    screen.fill(BLACK)
+    fichero=open('matriz_aleatoria.txt','r')
+    ventana.fill(BLACK)
     Fuente = pygame.font.SysFont('fontname', 16)
     Tx = 0
     Ty = 0
-    for i in range(1, size[0], 40):
+    for i in range(1, tamaño_ventana[0], 40):
     	linea = fichero.readline()
     	if linea != '':
     		count=0
-	    	for j in range(1, size[1], 40):
+	    	for j in range(1, tamaño_ventana[1], 40):
 	    		#print(count)
 	    		while linea[count]==',':
 	    			count=count+1
 	    		if linea[count] == '0':
-	    			pygame.draw.rect(screen, blueP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, greyP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '1':
-	    			pygame.draw.rect(screen, greenP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, brownP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '2':
-	    			pygame.draw.rect(screen, redP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, blueP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '3':
-	    			pygame.draw.rect(screen, pinkP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, yellowP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '4':
-	    			pygame.draw.rect(screen, greyP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, greenP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '5':
-	    			pygame.draw.rect(screen, aquaP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, aquaP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '6':
-	    			pygame.draw.rect(screen, yellowP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, pinkP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '7':
-	    			pygame.draw.rect(screen, purpleP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, purpleP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '8':
-	    			pygame.draw.rect(screen, whiteP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, whiteP, [j, i, 38, 38], 0)
 	    		elif linea[count] == '9':
-	    			pygame.draw.rect(screen, brownP, [j, i, 38, 38], 0)
+	    			pygame.draw.rect(ventana, redP, [j, i, 38, 38], 0)
 	    		count=count+1
     	count=0
     	Texto = Fuente.render(str(Tx), True, BLACK)
-    	screen.blit(Texto, [i, 2])
+    	ventana.blit(Texto, [i, 2])
     	if Tx != 0:
-    		screen.blit(Texto, [2, i + 16])
+    		ventana.blit(Texto, [2, i + 16])
     	Tx += 1
     	Ty = 0
     fichero.close()
--- genera_matriz.py
+++ genera_matriz.py
@@ -1,17 +1,36 @@
 import numpy as np
 
-def crear_matriz():
-    matriz = np.random.randint(0,9,(10,10),'int')
+def crear_matriz(probabilidades): ##Genera una matriz de numeros enteros aleatorios 
+    matriz = np.random.choice(5,(15,15),p=probabilidades)
     return matriz
 
-def guardar_matriz(fname, num_array):
-    a = open("test.txt", "w")
-    for fila in matriz:
-        np.savetxt(a, fila)
-    
+def guardar_matriz(fname, num_array): ##guarda la matriz :V
+    a = open(fname, "w")
+    np.savetxt(a,matriz,fmt='%d',delimiter=',')
+
     a.close()
+    
+def obtener_probabilidades(numero_probabilidades):
+    total = 1.0
+    probabilidad_restante = total
+    probabilidades = []
+    for i in range(numero_probabilidades):
+        if(i < numero_probabilidades-1):
+            probabilidad = probabilidad_restante  * np.random.random_sample() 
+            probabilidad_restante = probabilidad_restante - probabilidad
+            probabilidades.append(probabilidad)
+            '''print("probabilidad:",probabilidad)
+            print("probabilidad restante",probabilidad_restante)'''
+        else:
+            probabilidades.append(probabilidad_restante)
+            probabilidad_restante = 0
+            '''print("probabilidad:",probabilidad)
+            print("probabilidad restante",probabilidad_restante)'''
+        print(probabilidades)
 
 if __name__ == '__main__':
-    matriz = crear_matriz()
-    guardar_matriz("prueba.txt",matriz)
-
+    probabilidades= obtener_probabilidades(5)
+    fname = "matriz_aleatoria.txt"
+    matriz = crear_matriz(probabilidades)
+    guardar_matriz(fname,matriz)
+    print(np.loadtxt(fname,dtype=int,delimiter=','))
