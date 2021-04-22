665f133be738febf7e34eb782b01c36c12044ec7
nocsag <nocsag2@gmail.com>
2021-04-22 00:01:18 -0500

    actualización agente

--- agente.py
+++ agente.py
@@ -4,20 +4,28 @@ matriz = ma.cargar_matriz('matriz_aleatoria.txt')
 fil = matriz.shape[0]
 col = matriz.shape[1]
 
-params = {}             #Se crea el diccionario de parametros
+params = {}  # Se crea el diccionario de parametros
+paramsd = {}  # Se crea el diccionario de parametros
 
 for x in range(0, fil):
     for y in range(0, col):
         params[(x, y)] = (('V', False), ('O', False),   ##Se crea una lista de tuplas que definen los
-                          ('I', False), ('X', False))   ##parámetros de cada casilla y se asignan al
-        print(params[(x,y)])                                                ##diccionario según sus coordenadas
+                          ('I', False), ('X', False), ('S',False))
+
+params[(0, 5)] = (('V', False), ('O', False),   ##Se crea una lista de tuplas que definen los
+                          ('I', True), ('X', False), ('S',False))
 
 
-humano={0:False,1:1,2:2,3:3,4:4,5:5,6:5}
-
-print(matriz[0][0])
+humano = {0: False, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 5}
 
 def agente():
 
-
-    return param
+    for i in range(0, fil):
+        for j in range(0, col):
+            lista_params = params[(i, j)]
+            for param in lista_params:
+                if(param[0]=='I'):
+                   if(param[1]):
+                    print(param)
+                    print(i,j)
+agente()
