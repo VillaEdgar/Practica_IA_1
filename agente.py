f8d868519c85b5f8b3999a67e84d832c33e4e3e0
nocsag <nocsag2@gmail.com>
2021-04-23 08:16:31 -0500

    agente 0.2

--- agente.py
+++ agente.py
@@ -24,7 +24,7 @@ def spawn(paramsd,matriz):
 def sense(paramsd, matriz):
     col= matriz.shape[0]
     fil = matriz.shape[1]
-
+    aux=0;
     for i in range(0, fil):
         for j in range(0, col):
 
@@ -34,13 +34,26 @@ def sense(paramsd, matriz):
 
                 if(i>0):
                     paramsd[(i-1, j)]['S']= True
+                    if humano[matriz[i -1][j]]and not paramsd[(i-1, j)]['V']:
+                        aux = aux + 1
+
                 if(i<fil-1):
                     paramsd[(i+1,j)]['S'] = True
-                if ( j>0):
+                    if humano[matriz[i+1][j]]and not paramsd[(i+1, j)]['V']:
+                        aux = aux + 1
+
+                if (j>0):
                     paramsd[(i, j-1)]['S']= True
-                    print(j-1)
+                    if humano[matriz[i][j -1]] and not paramsd[(i, j-1)]['V']:
+                        aux = aux + 1
                 if (j < col-1):
                     paramsd[(i, j+1)]['S']= True
+                    if humano[matriz[i][j+1]]and not paramsd[(i, j+1)]['V']:
+                        aux = aux+1
+
+                if aux>1:
+                    paramsd[(i, j )]['O'] = True
+                    print(paramsd[(i, j )])
     return paramsd
 
 def step(paramsd, matriz):
@@ -53,31 +66,25 @@ def step(paramsd, matriz):
 
                     paramsd[(i, j)]['V'] = True
 
-                    if (i-1 > 0):
-                        if(humano[matriz[i-1][j]]):
-                            paramsd[(i, j)]['X']=False
-                            paramsd[(i-1, j)]['X']=True
-                            return paramsd
+                    if i-1 >= 0 and humano[matriz[i-1][j]] and not paramsd[(i-1, j)]['V']:
+                        paramsd[(i, j)]['X']=False
+                        paramsd[(i-1, j)]['X']=True
+                        return paramsd
 
-                    if (j - 1 > 0):
-                        if (humano[matriz[i - 1][j]]):
-                            paramsd[(i, j)]['X'] = False
-                            paramsd[(i, j - 1)]['X'] = True
+                    if j -1 >= 0 and humano[matriz[i][j-1]] and not paramsd[(i, j-1)]['V']:
+                        paramsd[(i, j)]['X'] = False
+                        paramsd[(i, j - 1)]['X'] = True
+                        return paramsd
 
-                            return paramsd
+                    if i+1 < fil-1 and humano[matriz[i+1][j]] and not paramsd[(i+1, j)]['V'] :
+                        paramsd[(i, j)]['X'] = False
+                        paramsd[(i +1, j)]['X'] = True
+                        return paramsd
 
-                    if(i+1 < fil-1 ):
-                        if (humano[matriz[i - 1][j]]):
-                            paramsd[(i, j)]['X'] = False
-                            paramsd[(i +1, j)]['X'] = True
-
-                            return paramsd
-
-                    if (j+1 < col-1):
-                        if (humano[matriz[i - 1][j]]):
-                            paramsd[(i, j)]['X'] = False
-                            paramsd[(i, j+1)]['X'] = True
-                            return paramsd
+                    if j+1 < col-1 and humano[matriz[i][j+1]] and not paramsd[(i, j+1)]['V']:
+                        paramsd[(i, j)]['X'] = False
+                        paramsd[(i, j+1)]['X'] = True
+                        return paramsd
 
 
 
