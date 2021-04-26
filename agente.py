0cc2baab557ca4eb153bf462c7f374c97bca83e4
VillaEdgar <edgar_a_villa@hotmail.com>
2021-04-26 14:04:05 -0500

    Interfaz funcional
    La interfaz funciona.

--- agente.py
+++ agente.py
@@ -1,19 +1,39 @@
 humano = {0: False, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 5} #definicion humano
-pulpo = {0: False, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 5} #definicion humano
+pulpo = {0: False, 1: 2, 2: 1, 3: False, 4: 3, 5: 2, 6: False} #definicion humano
+mono = {0: False, 1: 2, 2: 4, 3: 3, 4: 1, 5: 5, 6: False}
+chupacabras = {0: 15, 1: 4, 2: False, 3: False, 4: 4, 5: 5, 6: 3}
+ente={}
+
+
 
 '''for x in range(0, fil):
     print()
     for y in range(0, col):
         print(str(x),str(y), paramsd[(x, y)])
 '''
+def definirAgente(agente):
+    if agente==1:
+        ente=humano
+    elif agente==2:
+        ente=pulpo
+    elif agente==3:
+        ente = mono
+    elif agente==4:
+        ente =chupacabras
+    elif agente<1 and agente>4:
+        print("ente no definido")
+    return ente
 
-def spawn(paramsd,matriz):
+def spawn(paramsd,matriz, ente2):
+
+
+
     col= matriz.shape[0]
     fil = matriz.shape[1]
     for i in range(0, fil):
         for j in range(0, col):
             if(paramsd[(i,j)]['I']):
-                if (humano[matriz[i][j]]):#generalizar
+                if (ente2[matriz[i][j]]):#generalizar
                     paramsd[(i,j)]['S'] = True
                     paramsd[(i,j)]['X'] = True
                 else:
@@ -25,7 +45,7 @@ def spawn(paramsd,matriz):
                     paramsd[(i-1,j)]['X'] = True
 
             if (paramsd[(i, j)]['F']):
-                if (humano[matriz[i][j]]):
+                if (ente2[matriz[i][j]]):
 
                     paramsd[(i, j)]['S'] = True
                 else:
@@ -36,7 +56,7 @@ def spawn(paramsd,matriz):
     return paramsd
 
 
-def sense(paramsd, matriz):
+def sense(paramsd, matriz, ente2):
     col= matriz.shape[0]
     fil = matriz.shape[1]
     aux=0;
@@ -48,22 +68,22 @@ def sense(paramsd, matriz):
 
                 if(i>0):
                     paramsd[(i-1, j)]['S']= True
-                    if (humano[matriz[i -1][j]]and not paramsd[(i-1, j)]['V']):
+                    if (ente2[matriz[i -1][j]]and not paramsd[(i-1, j)]['V']):
                         aux = aux + 1
 
                 if(i<fil-1):
                     paramsd[(i+1,j)]['S'] = True
-                    if (humano[matriz[i+1][j]]and not paramsd[(i+1, j)]['V']):
+                    if (ente2[matriz[i+1][j]]and not paramsd[(i+1, j)]['V']):
                         aux = aux + 1
 
                 if (j>0):
                     paramsd[(i, j-1)]['S']= True
-                    if (humano[matriz[i][j -1]] and not paramsd[(i, j-1)]['V']):
+                    if (ente2[matriz[i][j -1]] and not paramsd[(i, j-1)]['V']):
                         aux = aux + 1
 
                 if (j < col-1):
                     paramsd[(i, j+1)]['S']= True
-                    if (humano[matriz[i][j+1]]and not paramsd[(i, j+1)]['V']):
+                    if (ente2[matriz[i][j+1]]and not paramsd[(i, j+1)]['V']):
                         aux = aux+1
 
                 if aux>1:
@@ -73,7 +93,7 @@ def sense(paramsd, matriz):
 
     return paramsd
 
-def step(paramsd, matriz):
+def step(paramsd, matriz, ente2):
 
     col= matriz.shape[0]
     fil = matriz.shape[1]
@@ -100,32 +120,32 @@ def step(paramsd, matriz):
 
                         paramsd[(i, j)]['V'] = True
 
-                        if i-1 >= 0 and humano[matriz[i-1][j]] and not paramsd[(i-1, j)]['V']:
+                        if i-1 >= 0 and ente2[matriz[i-1][j]] and not paramsd[(i-1, j)]['V']:
                             paramsd[(i, j)]['X']=False
                             paramsd[(i-1, j)]['X']=True
 
 
                             return paramsd
 
-                        if j -1 >= 0 and humano[matriz[i][j-1]] and not paramsd[(i, j-1)]['V']:
+                        if j -1 >= 0 and ente2[matriz[i][j-1]] and not paramsd[(i, j-1)]['V']:
                             paramsd[(i, j)]['X'] = False
                             paramsd[(i, j - 1)]['X'] = True
 
                             return paramsd
 
-                        if i+1 < fil and humano[matriz[i+1][j]] and not paramsd[(i+1, j)]['V'] :
+                        if i+1 < fil and ente2[matriz[i+1][j]] and not paramsd[(i+1, j)]['V'] :
                             paramsd[(i, j)]['X'] = False
                             paramsd[(i +1, j)]['X'] = True
 
                             return paramsd
 
-                        if j+1 < col and humano[matriz[i][j+1]] and not paramsd[(i, j+1)]['V']:
+                        if j+1 < col and ente2[matriz[i][j+1]] and not paramsd[(i, j+1)]['V']:
                             paramsd[(i, j)]['X'] = False
                             paramsd[(i, j+1)]['X'] = True
 
                             return paramsd
 
-def step_down(paramsd, matriz):
+def step_down(paramsd, matriz, ente2):
 
     col= matriz.shape[0]
     fil = matriz.shape[1]
@@ -141,12 +161,12 @@ def step_down(paramsd, matriz):
                     if not paramsd[(i, j)]['F']:
                         paramsd[(i, j)]['V'] = True
 
-                        if i+1 < fil and humano[matriz[i+1][j]]:
+                        if i+1 < fil and ente2[matriz[i+1][j]]:
                             paramsd[(i, j)]['X'] = False
                             paramsd[(i +1, j)]['X'] = True
                             return paramsd
 
-def step_up(paramsd, matriz):
+def step_up(paramsd, matriz, ente2):
 
     col= matriz.shape[0]
     fil = matriz.shape[1]
@@ -162,14 +182,14 @@ def step_up(paramsd, matriz):
                     if not paramsd[(i, j)]['F']:
                         paramsd[(i, j)]['V'] = True
 
-                        if i-1 >= 0 and humano[matriz[i-1][j]]:
+                        if i-1 >= 0 and ente2[matriz[i-1][j]]:
                             paramsd[(i, j)]['X']=False
                             paramsd[(i-1, j)]['X']=True
 
 
                             return paramsd
 
-def step_right(paramsd, matriz):
+def step_right(paramsd, matriz, ente2):
 
     col= matriz.shape[0]
     fil = matriz.shape[1]
@@ -185,13 +205,13 @@ def step_right(paramsd, matriz):
                     if not paramsd[(i, j)]['F']:
                         paramsd[(i, j)]['V'] = True
 
-                        if j+1 < col and humano[matriz[i][j+1]]:
+                        if j+1 < col and ente2[matriz[i][j+1]]:
                             paramsd[(i, j)]['X'] = False
                             paramsd[(i, j+1)]['X'] = True
 
                             return paramsd
 
-def step_left(paramsd, matriz):
+def step_left(paramsd, matriz, ente2):
 
     col= matriz.shape[0]
     fil = matriz.shape[1]
@@ -207,7 +227,7 @@ def step_left(paramsd, matriz):
                     if not paramsd[(i, j)]['F']:
                         paramsd[(i, j)]['V'] = True
 
-                        if j -1 >= 0 and humano[matriz[i][j-1]]:
+                        if j -1 >= 0 and ente2[matriz[i][j-1]]:
                             paramsd[(i, j)]['X'] = False
                             paramsd[(i, j - 1)]['X'] = True
 
---
+++ interfaz.py
@@ -0,0 +1,92 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+import dibujo 
+
+
+class MainWindow(tk.Frame):
+    def __init__(self, parent, *args,**kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+        self.parent.title("MINTRIS")
+        self.parent.geometry("525x380")
+        self.configure(bg='#6B0002')
+        self.filename = ""
+        self.var_key = StringVar()
+        self.var_iv = StringVar()
+        self.var_ente = IntVar()
+        self.var_modo = IntVar()
+        self.ivname=""
+        self.imagen = []
+        self.img_enc = []
+        self.cabecera = []
+    
+        lbl = tk.Label(self, text="M I N T R I S",font=('Arial',32,'bold italic')
+                        ,background='#6B0002', foreground = '#91722F')
+        lbl.grid(row=0,column=0,columnspan=4, padx=90, pady=15)        
+        
+        lblmod = tk.Label(self, text="Seleccione un personaje",
+                         font=('Arial',18,'bold italic')
+                        ,background='#6B0002', foreground = '#04D14C')
+        lblmod.grid(row=1,column=0,columnspan=2,padx =10 ,pady=15)
+        
+        
+        rad0 = Radiobutton(self,text='Humano', value=1,
+                           font=('Arial',14,'bold italic'), 
+                           background='#6B0002',foreground = '#968118',variable=self.var_ente)
+        rad0.grid(row=2, column=0)
+        
+        rad1 = Radiobutton(self,text='Mono', value=3,
+                           font=('Arial',14,'bold italic'), 
+                           background='#6B0002',foreground = '#968118',variable=self.var_ente)
+        rad1.grid(row=2, column=1) 
+        
+        rad2 = Radiobutton(self,text='Pulpo', value=2,
+                           font=('Arial',14,'bold italic'),
+                           background='#6B0002',foreground = '#968118',variable=self.var_ente)
+        rad2.grid(row=2, column=2)
+
+        rad3 = Radiobutton(self,text='Sasquach', value=4,
+                           font=('Arial',14,'bold italic'),
+                           background='#6B0002',foreground = '#968118',variable=self.var_ente)
+        rad3.grid(row=3, column=1, pady=15)
+        
+                
+        lblmod = tk.Label(self, text="Seleccione un modo",
+                         font=('Arial',18,'bold italic')
+                        ,background='#6B0002', foreground = '#04D14C')
+        lblmod.grid(row=4,column=0,columnspan=2 ,pady=15)
+        
+        
+        rad0 = Radiobutton(self,text='Manual', value=1,
+                           font=('Arial',14,'bold italic'), 
+                           background='#6B0002',foreground = '#968118',variable=self.var_modo)
+        rad0.grid(row=5, column=0)
+        
+        rad1 = Radiobutton(self,text='Automatico', value=2,
+                           font=('Arial',14,'bold italic'), 
+                           background='#6B0002',foreground = '#968118',variable=self.var_modo)
+        rad1.grid(row=5, column=1)
+    
+        
+        btn_Descifrar = tk.Button(self, text="Jugar", 
+                               bg='#4B7D23',width=10,height = 2, command=self.jugar,
+                               foreground='white',font=('Arial',10,'bold italic'))
+        btn_Descifrar.grid(row=7,column=1,pady=20)
+        
+        
+    def jugar(self):
+        ente = self.var_ente.get()
+        modo = self.var_modo.get()
+        if ente and modo:
+            dibujo.dibujar(ente,modo)
+        else:
+            messagebox.showinfo(message="Seleccione un personaje y un modo")
+        
+
+        
+        
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainWindow(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
