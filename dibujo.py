1433130e407764ce03ec0b97ce27d3d46ab418a1
VillaEdgar <edgar_a_villa@hotmail.com>
2021-04-27 18:31:29 -0500

    Add files via upload

--- dibujo.py
+++ dibujo.py
@@ -22,7 +22,7 @@ tamañoCasilla = 40
 # tamañoCuadricula es el numero de casillas que tendrá la cuadricula por lado
 tamañoCuadricula = 15
 columna = 0
-def dibujar(agente,modo,x,y):
+def dibujar(agente,modo,xx,yy):
     print("Agente"+str(agente))
     print("Modo"+str(modo))
     pygame.init()
@@ -57,7 +57,7 @@ def dibujar(agente,modo,x,y):
         for y in range(0, col):
             paramsd[(x, y)] = {'V': False, 'O': False, 'I': False, 'X': False, 'S':False, 'F':False, 'k':False}
 
-    paramsd[(x, y)] = {'V': False, 'O': False, 'I': True, 'X': False, 'S':False,'F':False}
+    paramsd[(xx, yy)] = {'V': False, 'O': False, 'I': True, 'X': False, 'S':False,'F':False}
     paramsd[(6,8)] = {'V': False, 'O': False, 'I': False, 'X': False, 'S':False,'F':True}
 
     ente=ag.definirAgente(agente)
